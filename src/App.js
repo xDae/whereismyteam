19fe21c38b1a4d5aa2c6f25698a4d64c4e7eb018
Jose Miguel Bejarano <yo@josemiguel.org>
2016-12-15 20:43:53 +0100

    💪🏻 lot of spaghetti code 😬

--- src/App.js
+++ src/App.js
@@ -1,33 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Rebase from 're-base';
 import Webcam from "react-user-media";
+import ago from 's-ago';
+
+import { apiKey, authDomain, databaseURL, storageBucket, messagingSenderId } from './firebase-config';
+import logo from './logo.svg';
+import './App.css';
 
 var base = Rebase.createClass({
-  apiKey: 'AIzaSyDh-01Z3gn5INCvcJ5n1cjkilfus_DeR_0',
-  authDomain: 'whereismyteam-caa8e.firebaseapp.com',
-  databaseURL: 'https://whereismyteam-caa8e.firebaseio.com',
-  storageBucket: 'whereismyteam-caa8e.appspot.com',
-  messagingSenderId: '159931923593'
-}, 'myApp');
+  apiKey, authDomain, databaseURL, storageBucket, messagingSenderId
+});
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      photo: ''
+      activeCam: true,
+      user: null,
+      photoURL: logo,
+      screenshot: {
+        screenshot: '',
+        date: ''
+      }
     };
   }
 
   componentDidMount(){
-    base.bindToState('photo', {
+    base.bindToState(`/EUlHQ4lVLidhkwBRbMBSqjijfGy2/`, {
       context: this,
-      state: 'photo',
+      state: 'screenshot',
       asArray: false
     });
 
-    setInterval(this.takeSnapshot, 60000);
+    if (this.state.user) {
+      setInterval(this.takeSnapshot(this.state.user.uid), 60000);
+    }
+
+
+    base.onAuth(this.authDataCallback);
+  }
+
+  authDataCallback = user => {
+    if (user) {
+      console.log("User " + user.uid + " is logged in with " + user.providerId);
+      this.setState({
+        user: {
+          name: user.displayName,
+          uid: user.uid,
+          providerId: user.providerId,
+        },
+        photoURL: user.photoURL
+      });
+    } else {
+      console.log("User is logged out");
+      this.setState({
+        user: null,
+        photoURL: logo
+      });
+    }
   }
 
   addItem = newItem => {
@@ -36,12 +66,15 @@ class App extends Component {
     });
   }
 
-  takeSnapshot = () => {
-    console.log()
-    const photo = this.refs.webcam.captureScreenshot();
+  takeSnapshot = user => {
+    console.log('snapshot taked!')
+    const screenshot = this.refs.webcam.captureScreenshot();
 
-    base.update('/', {
-      data: { photo }
+    base.update(`/${user}`, {
+      data: {
+        screenshot,
+        date: base.database.ServerValue.TIMESTAMP
+      }
     }).then(() => console.log('photo subida'));
   }
 
@@ -49,28 +82,55 @@ class App extends Component {
     console.log('webcam ON');
   }
 
+  register = () => {
+    var authHandler = (error, user) => {
+      if(error) console.log('EERROR', error);
+      console.log(user);
+    }
+    //basic
+    base.authWithOAuthPopup('google', authHandler);
+  }
+
+  renderName = () => {
+    if (this.state.user) {
+      return this.state.user.name;
+    }
+
+    return 'Anonymous';
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <img src={this.state.photoURL} className="App-logo" alt="logo" />
+          <h2>Welcome {this.renderName()}</h2>
         </div>
         <p className="App-intro">
-          <Webcam
-            width={300}
-            height={225}
-            ref="webcam"
-            audio={false}
-            onSuccess={this.uploadToFirebase}
-            onFailure={(error) => console.log('EEEERROR!: ', error)}
-          />
-          <img className="snapshot-img" src={this.state.photo} />
-          <button onClick={this.takeSnapshot}>take snapshot</button>
+          {this.state.activeCam && (
+            <Webcam
+              width={300}
+              height={225}
+              ref="webcam"
+              audio={false}
+              onSuccess={this.uploadToFirebase}
+              onFailure={(error) => console.log('EEEERROR!: ', error)}
+            />)
+          }
+          <img className="snapshot-img" src={this.state.screenshot.screenshot} />
+          <span>{ago(new Date(this.state.screenshot.date))}</span>
+          {!this.state.activeCam && (
+              <span>snapshot stopped</span>
+            )
+          }
+          <button onClick={() => this.takeSnapshot(this.state.user.uid)}>take snapshot</button>
+
+          <button onClick={this.register}>registro</button>
+          <button onClick={() => base.unauth()}>logout</button>
+          <button onClick={() => this.setState({activeCam: !this.state.activeCam})}>toggle cam</button>
         </p>
       </div>
     );
   }
 }
-
 export default App;
---
+++ src/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDh-01Z3gn5INCvcJ5n1cjkilfus_DeR_0",
+  authDomain: "whereismyteam-caa8e.firebaseapp.com",
+  databaseURL: "https://whereismyteam-caa8e.firebaseio.com",
+  storageBucket: "whereismyteam-caa8e.appspot.com",
+  messagingSenderId: "159931923593"
+};
+
+const { apiKey, authDomain, databaseURL, storageBucket, messagingSenderId } = firebaseConfig;
+
+export { apiKey, authDomain, databaseURL, storageBucket, messagingSenderId }
